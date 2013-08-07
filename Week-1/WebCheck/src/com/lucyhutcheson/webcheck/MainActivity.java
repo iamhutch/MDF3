0e763a2d6845900c16e35e1d9461b402599e90a1
Lucy Hutcheson <hutcheson.lucy@gmail.com>
2013-08-07 13:01:17 -0500

    Added WebLaunch for my WebCheck app

--- Week-1/WebCheck/src/com/lucyhutcheson/webcheck/MainActivity.java
+++ Week-1/WebCheck/src/com/lucyhutcheson/webcheck/MainActivity.java
@@ -1,15 +1,56 @@
+/*
+ * project		WebCheck
+ * 
+ * package		com.lucyhutcheson.webcheck
+ * 
+ * @author		Lucy Hutcheson
+ * 
+ * date			Aug 5, 2013
+ * 
+ */
 package com.lucyhutcheson.webcheck;
 
 import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.Window;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
+@SuppressLint("SetJavaScriptEnabled")
 public class MainActivity extends Activity {
 
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		WebView webview = new WebView(this);
+		setContentView(webview);
+		
+		 // Let's display the progress in the activity title bar, like the
+		 // browser app does.
+		 getWindow().requestFeature(Window.FEATURE_PROGRESS);
+
+		 webview.getSettings().setJavaScriptEnabled(true);
+
+		 final Activity activity = this;
+		 webview.setWebChromeClient(new WebChromeClient() {
+		   public void onProgressChanged(WebView view, int progress) {
+		     // Activities and WebViews measure progress with different scales.
+		     // The progress meter will automatically disappear when we reach 100%
+		     activity.setProgress(progress * 1000);
+		   }
+		 });
+		 webview.setWebViewClient(new WebViewClient() {
+		   public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+		     Toast.makeText(activity, "Oh no! " + description, Toast.LENGTH_SHORT).show();
+		   }
+		 });
+
+		 webview.loadUrl("http://slashdot.org/");
 	}
 
 	@Override
@@ -19,4 +60,6 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	
 }
+
---
+++ Week-1/WebLaunch/src/com/lucyhutcheson/weblaunch/MainActivity.java
@@ -0,0 +1,62 @@
+/*
+ * project		WebLaunch
+ * 
+ * package		com.lucyhutcheson.weblaunch
+ * 
+ * @author		Lucy Hutcheson
+ * 
+ * date			Aug 5, 2013
+ * 
+ */
+package com.lucyhutcheson.weblaunch;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		// VIEW WEBSITE BUTTON AND HANDLER FOR IMPLICIT INTENT
+		Button webButton = (Button) findViewById(R.id.webButton);
+		webButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+				EditText editURI = (EditText) findViewById(R.id.urlField);
+				Uri intentUri = Uri.parse(editURI.getText().toString());
+
+				// SETUP IMPLICIT INTENT
+				Intent webIntent = new Intent();
+				webIntent.setAction(Intent.ACTION_VIEW);
+				webIntent.setData(intentUri);
+				// startActivity(webIntent);
+
+				String title = "View this website with";
+				// Create and start the chooser
+				Intent chooser = Intent.createChooser(webIntent, title);
+				startActivity(chooser);
+
+			}
+		});
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
