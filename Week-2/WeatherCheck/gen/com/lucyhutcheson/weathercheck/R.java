db608d7f08d3e5023c23b1ebaa45db9bab460855
Lucy Hutcheson <hutcheson.lucy@gmail.com>
2013-08-14 21:34:42 -0500

    Fixed weather icon and layout.

--- Week-2/WeatherCheck/gen/com/lucyhutcheson/weathercheck/R.java
+++ Week-2/WeatherCheck/gen/com/lucyhutcheson/weathercheck/R.java
@@ -24,11 +24,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int _btnBigPic=0x7f080002;
-        public static final int _imageView=0x7f080003;
+        public static final int _btnBigPic=0x7f080003;
+        public static final int _imageView=0x7f080002;
+        public static final int _imageWeather=0x7f080000;
         public static final int _temp=0x7f080001;
         public static final int action_settings=0x7f080004;
-        public static final int weatherData=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -38,14 +38,13 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
-        public static final int album_name=0x7f050007;
+        public static final int album_name=0x7f050005;
         public static final int app_name=0x7f050000;
         public static final int btnBigPic=0x7f050003;
-        public static final int btnSmallPic=0x7f050004;
-        public static final int btnVideo=0x7f050005;
-        public static final int cannot=0x7f050006;
+        public static final int cannot=0x7f050004;
+        public static final int current_temp=0x7f050007;
         public static final int hello_world=0x7f050002;
-        public static final int image_taken=0x7f050008;
+        public static final int image_taken=0x7f050006;
     }
     public static final class style {
         /** 
