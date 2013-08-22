f92cd7f4a296c814ea41af111e5095826dc111d5
Lucy Hutcheson <hutcheson.lucy@gmail.com>
2013-08-22 18:41:10 -0500

    Fixed the graphics for the app widget

--- Week-3/ChristmasCountdown/gen/com/lucyhutcheson/christmascountdown/R.java
+++ Week-3/ChristmasCountdown/gen/com/lucyhutcheson/christmascountdown/R.java
@@ -11,7 +11,8 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int background=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
         public static final int daysLeft=0x7f070001;
@@ -23,6 +24,8 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f050000;
+        public static final int background=0x7f050001;
+        public static final int days_left_until_christmas=0x7f050002;
     }
     public static final class style {
         /** 
--- Week-3/ChristmasCountdown/src/com/lucyhutcheson/christmascountdown/CountdownWidgetProvider.java
+++ Week-3/ChristmasCountdown/src/com/lucyhutcheson/christmascountdown/CountdownWidgetProvider.java
@@ -1,6 +1,5 @@
 package com.lucyhutcheson.christmascountdown;
 
-import java.util.Calendar;
 
 import com.lucyhutcheson.christmascountdown.R;
 
@@ -10,9 +9,7 @@ import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.net.Uri;
-import android.os.CountDownTimer;
 import android.util.Log;
 import android.widget.RemoteViews;
 
